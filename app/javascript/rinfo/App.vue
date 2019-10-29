8a4f63cf7aa322ba0faa2259cb1e331717495e7d
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-10-29 17:55:47 +0900

    Use vue single file component

---
+++ app/javascript/rinfo/App.vue
@@ -0,0 +1,40 @@
+<template lang='pug'>
+div
+  div
+    textarea(v-model='sql')
+  button(@click='execute') execute
+
+  table.result(v-if='result')
+    thead
+      tr
+        th(v-for='column in result.columns') {{column}}
+    tbody
+      tr(v-for='row in result.rows')
+        td(v-for='value in row') {{value}}
+</template>
+
+<script>
+import axios from 'axios'
+
+axios.defaults.headers.common = {
+  'X-Requested-With': 'XMLHttpRequest',
+  'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+}
+
+export default {
+  data(){
+    return {
+      sql: 'select * from books',
+      result: {},
+    }
+  },
+  methods: {
+    async execute(){
+      const result = await axios.post('sql', {sql: this.sql})
+      console.log(result)
+      console.log(result.data)
+      this.result = result.data
+    },
+  },
+}
+</script>
--- app/javascript/rinfo/sql.js
+++ app/javascript/rinfo/sql.js
@@ -1,26 +1,10 @@
 import Vue from 'vue/dist/vue.common'
-import axios from 'axios'
 import 'regenerator-runtime/runtime'
-
-axios.defaults.headers.common = {
-  'X-Requested-With': 'XMLHttpRequest',
-  'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-}
+import App from './App'
 
 new Vue({
   el: '#app',
-  data(){
-    return {
-      sql: 'select * from books',
-      result: {},
-    }
-  },
-  methods: {
-    async execute(){
-      const result = await axios.post('sql', {sql: this.sql})
-      console.log(result)
-      console.log(result.data)
-      this.result = result.data
-    },
+  components: {
+    App,
   },
 })
