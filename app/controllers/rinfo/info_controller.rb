747b5913291673223d7104b06d96f359f68f04fd
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-10-29 15:10:37 +0900

    Add tables view

--- app/controllers/rinfo/info_controller.rb
+++ app/controllers/rinfo/info_controller.rb
@@ -6,6 +6,16 @@ module Rinfo
   class InfoController < ApplicationController
 
     def tables
+      internal_tables = %w(
+        schema_migrations
+        ar_internal_metadata
+        active_storage_attachments
+        active_storage_blobs
+      )
+      table_names = ActiveRecord::Base.connection.tables - internal_tables
+      @models = table_names.map do |table_name|
+        table_name.classify.constantize
+      end
     end
 
   end
