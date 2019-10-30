585c74f82507b74eaec27817958a227aeddca065
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-10-30 10:30:10 +0900

    Add assets precompile config

--- lib/rinfo/engine.rb
+++ lib/rinfo/engine.rb
@@ -5,5 +5,10 @@ require 'slim'
 module Rinfo
   class Engine < ::Rails::Engine
     isolate_namespace Rinfo
+
+    initializer 'rinfo.assets.precompile' do |app|
+      app.config.assets.precompile += ['rinfo/*.js']
+    end
+
   end
 end
