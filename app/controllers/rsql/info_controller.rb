b945b15fc607c359c0611bde134abb941e6e757e
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-10-30 19:55:47 +0900

    Remove tables view

--- app/controllers/rsql/info_controller.rb
+++ app/controllers/rsql/info_controller.rb
@@ -5,19 +5,6 @@ require_dependency 'rsql/application_controller'
 module Rsql
   class InfoController < ApplicationController
 
-    def tables
-      internal_tables = %w(
-        schema_migrations
-        ar_internal_metadata
-        active_storage_attachments
-        active_storage_blobs
-      )
-      table_names = ActiveRecord::Base.connection.tables - internal_tables
-      @models = table_names.map do |table_name|
-        table_name.classify.constantize
-      end
-    end
-
     def execute_sql
       result = ActiveRecord::Base.connection.exec_query(params[:sql])
       render json: {
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rsql::Engine.routes.draw do
-  get 'tables' => 'info#tables'
   get 'sql' => 'info#sql'
   post 'sql' => 'info#execute_sql'
 end
