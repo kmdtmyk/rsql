eac93524c0c976ae1de3eea2dcc5e81cebf7e7f9
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-10-31 16:46:07 +0900

    Add test of CRUD query

--- lib/rsql/query.rb
+++ lib/rsql/query.rb
@@ -4,24 +4,57 @@ module Rsql
   class Query
 
     def self.execute(sql)
-      new(sql).result
+      new(sql).execute
     end
 
     def initialize(sql)
       @sql = sql
     end
 
-    def result
-      result = ActiveRecord::Base.connection.exec_query(@sql)
-      {
-        columns: result.columns,
-        rows: result.rows,
-      }
+    def execute
+      if @sql.match? /insert[[:space:]]+into/i
+        execute_insert
+      elsif @sql.match? /update/i
+        execute_update
+      elsif @sql.match? /delete[[:space:]]+from/i
+        execute_delete
+      else
+        execute_query
+      end
     rescue => e
       {
         error: e.to_s
       }
     end
+
+    private
+
+      def execute_query
+        result = ActiveRecord::Base.connection.exec_query(@sql)
+        {
+          columns: result.columns,
+          rows: result.rows,
+          count: result.rows.size,
+        }
+      end
+
+      def execute_insert
+        {
+          count: ActiveRecord::Base.connection.insert(@sql),
+        }
+      end
+
+      def execute_update
+        {
+          count: ActiveRecord::Base.connection.update(@sql),
+        }
+      end
+
+      def execute_delete
+        {
+          count: ActiveRecord::Base.connection.delete(@sql),
+        }
+      end
 
   end
 end
--- spec/lib/rsql/query_spec.rb
+++ spec/lib/rsql/query_spec.rb
@@ -7,11 +7,94 @@ module Rsql
 
     describe 'execute' do
 
-      example 'select' do
-        Book.create
-        result = Query.execute('select * from books')
-        expect(result[:columns]).to eq Book.attribute_names
-        expect(result[:rows].size).to eq 1
+      describe 'select' do
+
+        before do
+          Book.create(id: 1)
+          Book.create(id: 2)
+          Book.create(id: 3)
+        end
+
+        example 'select' do
+          result = Query.execute('select * from books')
+          expect(result[:columns]).to eq Book.attribute_names
+          expect(result[:rows].size).to eq 3
+          expect(result[:count]).to eq 3
+        end
+
+        example '1 record' do
+          result = Query.execute('select * from books where id = 1')
+          expect(result[:columns]).to eq Book.attribute_names
+          expect(result[:rows].size).to eq 1
+          expect(result[:count]).to eq 1
+        end
+
+      end
+
+      describe 'insert' do
+
+        example 'ignore case' do
+          sql = "InSeRt InTo books (created_at, updated_at) values ('2019-10-15', '2019-10-15')"
+          result = Query.execute(sql)
+          expect(result[:count]).to eq 1
+        end
+
+        example 'separated by space' do
+          sql = "insert \r\n \t into books (created_at, updated_at) values ('2019-10-15', '2019-10-15')"
+          result = Query.execute(sql)
+          expect(result[:count]).to eq 1
+        end
+
+      end
+
+      describe 'update' do
+
+        before do
+          Book.create(id: 1)
+          Book.create(id: 2)
+          Book.create(id: 3)
+        end
+
+        example 'ignore case' do
+          sql = "UpDaTe books set name = 'new name'"
+          result = Query.execute(sql)
+          expect(result[:count]).to eq 3
+        end
+
+        example '1 record' do
+          sql = "update books set name = 'book1' where id = 1"
+          result = Query.execute(sql)
+          expect(result[:count]).to eq 1
+        end
+
+      end
+
+      describe 'delete' do
+
+        before do
+          Book.create(id: 1)
+          Book.create(id: 2)
+          Book.create(id: 3)
+        end
+
+        example 'ignore case' do
+          sql = "DeLeTe from books"
+          result = Query.execute(sql)
+          expect(result[:count]).to eq 3
+        end
+
+        example 'separated by space' do
+          sql = "delete \r\n \t from books"
+          result = Query.execute(sql)
+          expect(result[:count]).to eq 3
+        end
+
+        example '1 record' do
+          sql = "delete from books where id = 1"
+          result = Query.execute(sql)
+          expect(result[:count]).to eq 1
+        end
+
       end
 
       example 'syntax error' do
