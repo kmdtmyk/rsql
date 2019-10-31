02d48895ed73cb6a99b4aa4086694feefba5eb0c
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-10-31 13:48:07 +0900

    Add view helper for include javascript tag

--- app/helpers/rsql/application_helper.rb
+++ app/helpers/rsql/application_helper.rb
@@ -1,4 +1,18 @@
+# frozen_string_literal: true
+
 module Rsql
   module ApplicationHelper
+
+    def dummy_app?
+      Rails.app_class.to_s.start_with? 'Dummy'
+    end
+
+    def webpack_javascript_tag(name)
+      if dummy_app?
+        name += '.dev'
+      end
+      javascript_include_tag "rsql/#{name}"
+    end
+
   end
 end
