7f76294d63918fb0c6c601a0b660277f0aa4ab47
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-11-05 17:06:42 +0900

    Add test case that text contains sql comment

--- app/javascript/rsql/lib/SQLText.js
+++ app/javascript/rsql/lib/SQLText.js
@@ -1,9 +1,46 @@
 export default class{
 
   static split(text){
-    return text
-      .split(';')
+
+    const splitPositions = []
+    let lineComment = false
+    let commentLevel = 0
+
+    for(let i = 0; i < text.length; i++){
+      const char = text[i]
+      const char2 = text.substr(i, 2)
+      if(char2 === '--'){
+        i++
+        lineComment = true
+      }else if(char2 === '/*'){
+        i++
+        commentLevel++
+      }else if(char2 === '*/'){
+        i++
+        commentLevel--
+      }else if(char === '\n'){
+        lineComment = false
+      }else if(char === ';'){
+        if(lineComment === false && commentLevel === 0){
+          splitPositions.push(i + 1)
+        }
+      }
+    }
+
+    return splitByPosition(text, splitPositions)
       .filter(text => text.match(/[\S]/))
   }
 
+}
+
+function splitByPosition(text, positions){
+  const result = []
+  for(let i = 0; i < positions.length; i++){
+    const start = positions[i - 1] || 0
+    const end = positions[i]
+    result.push(text.substring(start, end))
+  }
+  const lastPosition = positions[positions.length - 1]
+  result.push(text.substring(lastPosition))
+  return result
 }
--- app/javascript/rsql/lib/SQLText.test.js
+++ app/javascript/rsql/lib/SQLText.test.js
@@ -18,14 +18,32 @@ describe('split', () => {
       'select * from table1;',
       'select * from table2',
     )
-    expect(SQLText.split(text)).toEqual(['select * from table1', '\nselect * from table2'])
+    expect(SQLText.split(text)).toEqual(['select * from table1;', '\nselect * from table2'])
+  })
+
+  test('-- comment' , () => {
+    const text = s(
+      'select * from table1;',
+      '--;',
+      'select * from table2',
+    )
+    expect(SQLText.split(text)).toEqual(['select * from table1;', '\n--;\nselect * from table2'])
+  })
+
+  test('/* comment */' , () => {
+    const text = s(
+      'select * from table1;',
+      '/* ; */',
+      'select * from table2',
+    )
+    expect(SQLText.split(text)).toEqual(['select * from table1;', '\n/* ; */\nselect * from table2'])
   })
 
   test('ignore last space' , () => {
     const text = s(
       'select * from table1; \t \n',
     )
-    expect(SQLText.split(text)).toEqual(['select * from table1'])
+    expect(SQLText.split(text)).toEqual(['select * from table1;'])
   })
 
 })
