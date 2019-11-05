71633b1c849a4fe06daf0f192d283109d3cbe629
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-11-05 17:54:54 +0900

    Change algorithm of get cursor query

--- app/javascript/rsql/lib/EditorText.js
+++ app/javascript/rsql/lib/EditorText.js
@@ -1,95 +1,37 @@
+import SQLText from './SQLText'
+
 export default class{
 
   static getCursorQuery(text, position){
+    const queries = SQLText.split(text)
     const lines = text.split('\n')
-    let array = []
 
-    const cursorLine = lines[position.row]
-    let cursorLeft = deleteLeftQuery(cursorLine.substring(0, position.column))
-    let cursorRight = deleteRightQuery(cursorLine.substring(position.column))
+    let cursorLength = 0
 
-    array.push(cursorLeft + cursorRight)
-
-    for(let i = position.row; 0 <= i; i--){
-      let line = lines[i]
+    for(let i = 0; i <= position.row; i++){
       if(i === position.row){
-        line = line.substring(0, position.column)
-        if(endOfQuery(line)){
-          break
-        }
-        continue
+        cursorLength += position.column
+      }else{
+        cursorLength += lines[i].length + 1
       }
-
-      if(endOfQuery(line)){
-        array.unshift(deleteLeftQuery(line))
-        break
-      }
-      array.unshift(line)
     }
 
-    for(let i = position.row; i < lines.length; i++){
-      let line = lines[i]
-      if(i === position.row){
-        line = line.substring(position.column)
-        if(endOfQuery(line)){
-          break
-        }
-        continue
-      }
-
-      if(endOfQuery(line)){
-        array.push(deleteRightQuery(line))
+    let count = 0
+    let result = queries[0] || ''
+    for(let i = 0; i < queries.length; i++){
+      const query = queries[i]
+      if(count <= cursorLength && cursorLength < count + query.length){
+        result = query
         break
       }
-
-      array.push(line)
+      count += query.length
     }
 
-    return array.join('\n')
+    if(result.endsWith(';')){
+      result = result.slice(0, -1)
+    }
+
+    return result
   }
 
-}
-
-function deleteLeftQuery(text){
-  const comment = getComment(text)
-  if(comment !== ''){
-    return deleteLeftQuery(deleteComment(text)) + comment
-  }
-  const index = text.lastIndexOf(';')
-  if(index !== -1){
-    return text.substring(index + 1)
-  }
-  return text
-}
-
-function deleteRightQuery(text){
-  const comment = getComment(text)
-  if(comment !== ''){
-    return deleteRightQuery(deleteComment(text)) + comment
-  }
-  const index = text.indexOf(';')
-  if(index !== -1){
-    return text.substring(0, index)
-  }
-  return text
-}
-
-function endOfQuery(line){
-  return deleteComment(line).includes(';')
-}
-
-function getComment(line){
-  const commentIndex = line.indexOf('--')
-  if(commentIndex !== -1){
-    return line.substring(commentIndex)
-  }
-  return ''
-}
-
-function deleteComment(line){
-  const commentIndex = line.indexOf('--')
-  if(commentIndex !== -1){
-    return line.substring(0, commentIndex)
-  }
-  return line
 }
